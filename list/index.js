2f13d3d34440b1ca390aaec4a8bbd83a4bad970f
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-14 19:53:05 +0100

    Node JS Command Line Finished

--- list/index.js
+++ list/index.js
@@ -1,8 +1,17 @@
 #!usr/bin/env node
 
 const fs = require('fs');
+const util = require('util'); //needed for the second and third resolution
+const chalk = require('chalk');
+const path = require ('path');
+//const lstat = util.promisify(fs.lstat); //needed for the second resolution
 
-fs.readdir(process.cwd(),(err, filenames)=> {
+//Method 3
+const { lstat } = fs.promises; 
+
+const targetDir = process.argv[2] || process.cwd(); 
+
+fs.readdir(targetDir, async(err, filenames)=> {
     //Either
     //err === an error object, which means something went wrong
     //OR
@@ -13,5 +22,73 @@ fs.readdir(process.cwd(),(err, filenames)=> {
         //throw new Error(err)
     }
 
-    console.log(filenames);
+    //FOR...OF LOOP FOR SOLUTION 2
+    // for (let filename of filenames) {
+    //     try {
+    //         const stats = await lstat(filename); 
+
+    //         console.log(filename, stats.isFile()); 
+    //     } catch (err) {
+    //         console.log(err)
+    //     }
+        
+    // }
+//ONE OPTION OF RESOLVING
+//   const allStats = Array(filenames.length).fill(null);
+
+//     for (let filename of filenames){
+//         const index = filename.indexOf(filename);
+//         fs.lstat(filename, (err,stats) => {
+//             if(err){
+//                 console.log(err);
+//             }
+
+//             allStats[index] = stats;
+
+//             const ready = allStats.every((stats) => {
+//                 return stats;
+//             });
+
+//             if(ready) {
+//                 allStats.forEach((stats, index) => {
+//                     console.log(filenames[index], stats.isFile() )
+//                 })
+//             }
+//         })
+//     }
+
+//SECOND OPTION OF RESOLVING THE PROBLEM
+
+// const lstat = (filename) => {
+//     return new Prommise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if(err){
+//                 reject(err); 
+//             }
+
+//             resolve(stats);
+//         })
+//     })
+// }
+
+//THIRD SOLUTION
+
+const statPromises = filenames.map(filename => {
+    return lstat(path.join(targetDir, filename)); 
+})
+
+const allStats = await Promise.all(statPromises);
+
+for(let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    console.log(filenames[index]);
+//FOR USING CHALK NPM
+    // if(stats.isFile()){
+    //     console.log(filenames[index])
+    // } else {
+    //    console.log(chalk.blue(filenames[index]))
+    // }
+}
+  
 });
