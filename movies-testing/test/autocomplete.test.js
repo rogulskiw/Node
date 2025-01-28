f5ee7ea875a4e97181bd7e6f9e7ff9208ba341e7
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-28 22:45:04 +0100

    Finishing testing app based on Mocha

--- movies-testing/test/autocomplete.test.js
+++ movies-testing/test/autocomplete.test.js
@@ -1,4 +1,20 @@
-beforeEach(()=> {
+const waitFor = (selector) => {
+    return new Promise((resolve, reject) => {
+        const interval = setInterval(() => {
+            if(document.querySelector(selector)){
+                clearInterval(interval);
+                clearTimeout(timeout);
+                resolve()
+            }
+        },30);
+        const timeout = setTimeout(() => {
+            clearInterval(interval);
+            reject();
+        },2000);
+    });
+};
+
+beforeEach(() => {
     document.querySelector('#target').innerHTML = '';
     createAutoComplete({
         root: document.querySelector('#target'), 
@@ -32,3 +48,15 @@ it('After searching, dropdown opens up', async () => {
 
     expect(dropdown.className).to.include('is-active');
 });
+
+it('After searching, displays some results', async () => {
+    const input = document.querySelector('input');
+    input.value = 'avengers';
+    input.dispatchEvent(new Event('input'));
+
+    await waitFor('.dropdown-item');
+
+    const items = document.querySelectorAll('.dropdown-item');
+
+    expect(items.length).to.equal(3); 
+})
