69ae496e9e5dce2bc205dcff6f02b06c542307e1
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-28 22:25:33 +0100

    Chai implementation

--- movies-testing/test/autocomplete.test.js
+++ movies-testing/test/autocomplete.test.js
@@ -1,3 +1,34 @@
-it('works!!', () => {
-    
+beforeEach(()=> {
+    document.querySelector('#target').innerHTML = '';
+    createAutoComplete({
+        root: document.querySelector('#target'), 
+        fetchData() {
+            return [
+                { Title: 'Avengers'},
+                { Title: 'Not Avengers'},
+                { Title: 'Some other Movie'}
+            ];
+        },
+        renderOption(movie) {
+            return movie.Title;
+        }
+    }); 
+})
+
+it('Dropdown starts closed', () => {
+    const dropdown = document.querySelector('.dropdown');
+
+    expect(dropdown.className).not.to.include('is-active');
+});
+
+it('After searching, dropdown opens up', async () => {
+    const input = document.querySelector('input');
+    input.value = 'avengers';
+    input.dispatchEvent(new Event('input'));
+
+    await waitFor('.dropdown-item');
+
+    const dropdown = document.querySelector('.dropdown');
+
+    expect(dropdown.className).to.include('is-active');
 });
