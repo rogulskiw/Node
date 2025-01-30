3dab594eacfbfbe0588c381866d236ab05e8e2c8
rogulski <131556841+rogulskiw@users.noreply.github.com>
2025-01-30 19:15:25 +0100

    Adding chalk and basic reporting

--- tme/runner.js
+++ tme/runner.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
+
+const forbiddenDirs = ['node_modules'];
 
 class Runner {
     constructor(){
@@ -8,7 +11,30 @@ class Runner {
 
     async runTest() {
         for (let file of this.testFiles) {
-            require(file.name); 
+            console.log(chalk.gray(`------- ${file.shortName}`))
+            const beforeEaches = [];
+            global.beforeEach = (fn) => {
+                beforeEaches.push(fn);
+            }
+            global.it = (desc, fn) => {
+                beforeEaches.forEach(func => func()); 
+                try {
+                    fn();
+                    console.log(chalk.green(`\tOK - ${desc}`));
+                } catch (err) {
+                    const message = err.message.replace(/\n/g, '\n\t\t')
+                    console.log(chalk.red(`\tX - ${desc}`));
+                    console.log(chalk.red('\t', message));
+                }
+                
+            }
+            try {
+                require(file.name); 
+            } catch {
+                console.log(`X - Error Loading File`, file.name)
+                console.log(chalk.red(err));
+            }
+            
         }
     }
 
@@ -20,8 +46,8 @@ class Runner {
             const stats = await fs.promises.lstat(filepath);
 
             if(stats.isFile() && file.includes('.test.js')){
-                this.testFiles.push({ name: filepath })
-            } else if(stats.isDirectory()){
+                this.testFiles.push({ name: filepath, shortName: file  })
+            } else if(stats.isDirectory() && !forbiddenDirs.includes(file)){
                 const childFiles = await fs.promises.readdir(filepath);
 
                 files.push(...childFiles.map(f => path.join(file, f)));
